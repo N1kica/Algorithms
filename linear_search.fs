5e97d26fad13d18ab0c62a240f0bd28f48ebdb78
N1kica <68340084+N1kica@users.noreply.github.com>
2023-12-12 08:58:33 +0100

    Update linear_search.fs

--- linear_search.fs
+++ linear_search.fs
@@ -6,13 +6,3 @@ let rec linear_search (haystack: 'T array) needle index =
             linear_search haystack needle (index + 1)
     else
         None
-
-[<EntryPoint>]
-let main arg =
-    let haystack = [| 5; 3; 15; 10; 16; 19; 17; 33; 35; 34 |]
-    let needle = 34
-
-    match linear_search haystack needle 0 with 
-    | Some(result) -> printfn "Needle found at: %d" result
-    | None -> printfn "Needle Not Found"
-    0
